a92b091f9cb3ab5808f1baa10c2a4f3b8f77b417
malek yagoub <malek.yagoub@hotmail.fr>
2018-01-11 15:01:26 +0100

    db script fix

--- sql/db/roille.sql
+++ sql/db/roille.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `client`
 --
 
+USE roille;
+
 DROP TABLE IF EXISTS `client`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
